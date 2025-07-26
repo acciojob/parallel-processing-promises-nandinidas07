e19f037d107db107252fa8532c0a4f3ea8eb1418
nandinidas07 <nandinidas107@gmail.com>
2025-07-26 21:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
+const loading=document.getElementById("loading");
+const errorDiv=document.getElementById("error");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-function downloadImage(url) {
+	function downloadImage(url) {
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.onload = () => resolve(img);
