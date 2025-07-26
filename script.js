1d3d1558adf4633edab3c42575fb54e79982b4b5
nandinidas07 <nandinidas107@gmail.com>
2025-07-26 21:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(Failed to load image: ${url});
+    img.src = url;
+  });
+}
+
+btn.addEventListener("click", () => {
+  output.innerHTML = "";
+  errorDiv.innerText = "";
+  loading.style.display = "block";
+
+  const promises = images.map(imgObj => downloadImage(imgObj.url));
+
+  Promise.all(promises)
+    .then(imgElements => {
+      imgElements.forEach(img => output.appendChild(img));
+    })
+    .catch(error => {
+      errorDiv.innerText = error;
+    })
+    .finally(() => {
+      loading.style.display = "none";
+    });
+});
